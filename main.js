e8ba7eb03830c6c8a37a0104803d4fa4e9f0a6d2
cotora <cotora2121ia@gmail.com>
2022-11-09 12:40:10 +0900

    delete debug code in main.js

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const resultPreview=document.getElementById("preview");
 inputCode.addEventListener("input",
 (e)=>{
     const result="\\("+inputCode.value+"\\)";
-    console.log(result);
     resultPreview.textContent=result;
     MathJax.typeset([resultPreview]);
 });
